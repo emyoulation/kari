3a27f5504920262fa2fd2eb8ae6be0cb0498bc61
Emyoulation or BAMaustin <69127217+emyoulation@users.noreply.github.com>
2023-08-17 21:08:50 -0500

    Update FilterParams.gpr.py
    update Gramps Plugin Registration for 5.2 compatibility

--- addons/FilterParams/FilterParams.gpr.py
+++ addons/FilterParams/FilterParams.gpr.py
@@ -1,5 +1,6 @@
 #
-# Gramps - a GTK+/GNOME based genealogy program
+# "FilterParams" tool, a modular plugin for Gramps 
+# (Gramps - the genealogy software suite built on GTK+/GNOME) 
 #
 # Copyright (C) 2021-2022      Gramps developers, Kari Kujansuu
 #
@@ -17,13 +18,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-
+# To contact the Isotammi Project members, visit the Taapeli GitHub repository:
+# https://github.com/Taapeli/isotammi-addons
+#
 
 """
 Gramps registration file
 """
 from gramps.gui import plug
 from gramps.version import major_version
+MODULE_VERSION = "5.2"
 
 plug.tool.tool_categories["Isotammi"] = ("Isotammi", _("Isotammi tools"))
 
@@ -39,11 +43,15 @@ register(
     name=_("FilterParams"),
     description=_("Display custom filters and allow changing their parameters"),
     version="1.1.6",
-    gramps_target_version=major_version,
-    status=STABLE,
+    gramps_target_version=MODULE_VERSION,
+    status=EXPERIMENTAL,
+    audience=EXPERT,
+    maintainers=["Kari Kujansuu"],
+    maintainers_email=["kari.kujansuu@gmail.com"],
     fname="FilterParams.py",
     authors=["Kari Kujansuu"],
     category="Isotammi",
+    help_url="https://github.com/emyoulation/kari/tree/maintenance/gramps52/addons/FilterParams",
     toolclass="Tool",
     optionclass="Options",
     tool_modes=[TOOL_MODE_GUI],
